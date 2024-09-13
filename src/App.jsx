e5a7b6e6747a517db65766224cfa15b91ef86068
EkanshM18 <ekanshmaheshwari27@gmail.com>
2024-09-13 16:43:37 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Landing from "./pages/Landing";
+import Product from "./pages/product";
+
+
 function App() {
   return(
     <div> 
      <Landing/>
+     <Product/>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Card(props) {
+    return(
+        <div className="p-4 bg-white m-4">
+           <div className="font-bold">{props.title}</div> 
+           <div className="">{props.subtitle}</div>
+           <div className="text-blue-500 hover:underline">{props.link}</div>
+        </div>
+    )
+}
